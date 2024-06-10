b2cba51e40334a96a424cb9c60abfee40177d990
Pranav Chandane <pranavhchandane@gmail.com>
2024-06-10 15:50:04 +0530

    Tried handling resizing. Refactored some code.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ use task::Task;
 use ui::UI;
 
 use pancurses::{
-    curs_set, endwin, init_pair, initscr, newwin, noecho, start_color, Input, Window, COLOR_BLACK,
-    COLOR_RED, COLOR_WHITE, COLOR_YELLOW,
+    curs_set, endwin, init_pair, initscr, newwin, noecho, resize_term, start_color, Input, Window,
+    COLOR_BLACK, COLOR_RED, COLOR_WHITE, COLOR_YELLOW,
 };
 use std::fs::{self, File};
 use std::process::exit;
@@ -137,6 +137,39 @@ fn create_window() -> Window {
     win
 }
 
+fn draw_ui(win: &Window, tasks: &mut Vec<Task>, curr_id: usize) {
+    let mut ui: UI = UI::default();
+
+    win.erase();
+    ui.begin(0);
+
+    if tasks.len() > 0 {
+        ui.label(
+            &win,
+            &format!(" [↑/↓] ALL TASKS: {} ", tasks.len()),
+            ui::TITLE_PAIR,
+        );
+    }
+
+    ui.label(&win, "\n", ui::REGULAR_PAIR);
+    ui.begin_list(curr_id);
+
+    if tasks.len() > 0 {
+        for (index, task) in tasks.iter().enumerate() {
+            ui.print_element(&win, &format!(" [{}] {} ", task.status, task.text), index);
+        }
+    } else {
+        ui.label(&win, " <-- CONTROLS --> ", ui::TITLE_PAIR);
+        ui.label(&win, "\n", ui::REGULAR_PAIR);
+        ui.label(&win, " <shift + a> : Add a new task", ui::UNIQUE_PAIR);
+        ui.label(&win, " <shift + t> : Toggle task status", ui::UNIQUE_PAIR);
+        ui.label(&win, " <shift + d> : Delete Task", ui::UNIQUE_PAIR);
+        ui.label(&win, " <shift + q> : Save & Exit", ui::UNIQUE_PAIR);
+    }
+
+    ui.end_list();
+}
+
 fn main() {
     match std::fs::File::open(FILE_PATH) {
         Ok(file) => file,
@@ -147,47 +180,19 @@ fn main() {
     handle_args(&mut tasks);
 
     let win: Window = create_window();
-    let mut ui: UI = UI::default();
-
     let mut curr_id: usize = 0;
     let mut quit: bool = false;
 
     while !quit {
-        win.erase();
-        ui.begin(0);
-
-        {
-            if tasks.len() > 0 {
-                ui.label(
-                    &win,
-                    &format!(" [↑/↓] ALL TASKS: {} ", tasks.len()),
-                    ui::TITLE_PAIR,
-                );
-            }
-
-            ui.label(&win, "\n", ui::REGULAR_PAIR);
-            ui.begin_list(curr_id);
-
-            if tasks.len() > 0 {
-                for (index, task) in tasks.iter().enumerate() {
-                    ui.print_element(&win, &format!(" [{}] {} ", task.status, task.text), index);
-                }
-            } else {
-                ui.label(&win, " <-- CONTROLS --> ", ui::TITLE_PAIR);
-                ui.label(&win, "\n", ui::REGULAR_PAIR);
-                ui.label(&win, " <shift + a> : Add a new task", ui::UNIQUE_PAIR);
-                ui.label(&win, " <shift + t> : Toggle task status", ui::UNIQUE_PAIR);
-                ui.label(&win, " <shift + d> : Delete Task", ui::UNIQUE_PAIR);
-                ui.label(&win, " <shift + q> : Save & Exit", ui::UNIQUE_PAIR);
-            }
-
-            ui.end_list();
-        }
-
-        win.refresh();
+        draw_ui(&win, &mut tasks, curr_id);
 
         match win.getch() {
-            // capital keys give an effect of composed keys (like shift + key)
+            // tried my best to resize the window but it's not that well
+            Some(Input::KeyResize | Input::Character('r')) => {
+                win.clear();
+                resize_term(0, 0);
+                win.refresh();
+            }
             Some(Input::Character('A')) => add_task(&mut tasks),
             Some(Input::Character('D')) => remove_task(&mut tasks, &mut curr_id),
             Some(Input::Character('T' | '\n')) => toggle_status(&mut tasks, &curr_id),
